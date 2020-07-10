fbabd8d75658a4d88d42f2e954902994c3d1029b
6sRyuSK <zikkyou19@gmail.com>
2020-07-10 09:07:08 +0900

    lint fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,31 +1,31 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
   },
   extends: [
     'plugin:vue/essential',
     '@vue/standard',
-    '@vue/typescript/recommended'
+    '@vue/typescript/recommended',
   ],
   parserOptions: {
-    ecmaVersion: 2020
+    ecmaVersion: 2020,
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'space-before-function-paren': ['error', 'never'],
-    'keyword-spacing': ['error', { 
-      "overrides": {
-        "if": { "after": false }
-      }
+    'keyword-spacing': ['error', {
+      overrides: {
+        if: { after: false },
+      },
     }],
-    'comma-dangle': ["error", {
-      'arrays': 'always-multiline',
-      'objects': 'always-multiline',
-      'imports': 'never',
-      'exports': 'never',
-      'functions': 'never',
-    }]
-  }
+    'comma-dangle': ['error', {
+      arrays: 'always-multiline',
+      objects: 'always-multiline',
+      imports: 'never',
+      exports: 'never',
+      functions: 'never',
+    }],
+  },
 }
